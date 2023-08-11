d4ec4f89421781bf4b258b795dcef49f5e2cce48
Sorin <sorin@q3.ro>
2023-08-11 15:23:00 +0300

    get query param, add api key to env var file

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server'
  
-export async function GET() {
-  const response = await fetch("https://www.omdbapi.com/?s=Matrix&apikey=720c3666");
+export async function GET(request: { url: string | URL; }) {
+  const { searchParams } = new URL(request.url);
+  const q = searchParams.get('q')
+  const response = await fetch(`https://www.omdbapi.com/?s=${q}&apikey=${process.env.API_KEY}`);
   const results = await response.json();
   return NextResponse.json(results.Search)
 }
