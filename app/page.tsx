e08d776aa398e23e669bddc127cb197e38c61392
Sorin <sorin@q3.ro>
2023-08-11 18:56:43 +0300

    display year & imdb id and link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 import { useState } from "react"
+import { FaImdb } from "react-icons/fa";
+import { PiClockDuotone } from "react-icons/pi";
 import Image from 'next/image'
 
 interface Movie {
   Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
   Poster: string;
 }
 
@@ -28,6 +33,12 @@ export default function Home() {
             <div className="my-2 flex p-2 bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md cursor-pointer">
               <img src={`${r.Poster}`} className="w-10 h-10 rounded mr-2" alt={`Poster for ${r.Title}`} />
               <div className="flex-grow">{r.Title}</div>
+              <div className="flex-initial px-5"><PiClockDuotone /> {r.Year}</div>
+              <div className="flex-initial px-5">
+                <a href={`https://www.imdb.com/title/${r.imdbID}/`} target="_blank">
+                  <span className="inline-block"><FaImdb /> {r.imdbID}</span>
+                </a>
+              </div>
               <div className="pl-4 cursor-move text-2xl text-gray-400 hover:text-gray-700 drag"><i className="bi bi-list"/></div>
             </div>
           </div>
