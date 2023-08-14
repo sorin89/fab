a431f1adec332dddf86c681dc83eb367635dce17
Sorin <sorin@q3.ro>
2023-08-15 00:47:45 +0300

    loading animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import { FaImdb } from "react-icons/fa";
 import { BiSun, BiMoon, BiSortAlt2 } from "react-icons/bi";
 import { PiClockDuotone } from "react-icons/pi";
+import { CgSpinner } from "react-icons/cg";
 import { HiViewList, HiViewGrid } from "react-icons/hi";
 import { Button, IconButton, ButtonGroup, Input, Stack, Text, useColorMode, HStack, Box, Flex, Select } from '@chakra-ui/react'
 import Image from 'next/image'
@@ -19,12 +20,15 @@ export default function Home() {
   const [results, setResults] = useState<Movie[]>([])
   const [query, setQuery] = useState<string>('')
   const [view, setView] = useState<string>('list')
+  const [loading, setLoading] = useState<boolean>(false)
 
   async function search(q: string) {
+    setLoading(true)
     setQuery(q)
     const response = await fetch(`/api/search?q=${q}`);
     const results = await response.json();
     setResults(results)
+    setLoading(false)
   }
 
   function setSearch(value: string) {
@@ -55,49 +59,55 @@ export default function Home() {
         className="my-10"
       />
       <div>
-        {results && results.length != 0 &&
-          <div>
-            <Box className="rounded-xl" bg={colorMode === 'light' ? 'gray.50' : 'gray.700' }>
-              <Flex>
-                <Box flex="1" className="flex p-4 items-center">{results.length} results</Box>
-                <Box className="p-4">
-                  <HStack>
-                    <Select icon={<BiSortAlt2/>}>
-                      <option>Default</option>
-                      <option>Title (A&raquo;Z)</option>
-                      <option>Title (Z&raquo;A)</option>
-                      <option>Year</option>
-                      <option>Type</option>
-                    </Select>
-                  </HStack>
+        {loading ?
+          <CgSpinner className="animate-spin text-4xl"/>
+        :
+          <>
+            {results && results.length != 0 &&
+              <div>
+                <Box className="rounded-xl" bg={colorMode === 'light' ? 'gray.50' : 'gray.700' }>
+                  <Flex>
+                    <Box flex="1" className="flex p-4 items-center">{results.length} results</Box>
+                    <Box className="p-4">
+                      <HStack>
+                        <Select icon={<BiSortAlt2/>}>
+                          <option>Default</option>
+                          <option>Title (A&raquo;Z)</option>
+                          <option>Title (Z&raquo;A)</option>
+                          <option>Year</option>
+                          <option>Type</option>
+                        </Select>
+                      </HStack>
+                    </Box>
+                    <Box className="p-4 rounded-r-xl">
+                      <ButtonGroup isAttached variant="outline">
+                        <IconButton isActive={view == 'list'} onClick={() => setView('list')} aria-label='List view' icon={<HiViewList/>} />
+                        <IconButton isActive={view == 'grid'} onClick={() => setView('grid')} aria-label='Grid View' icon={<HiViewGrid/>} />
+                      </ButtonGroup>
+                    </Box>
+                  </Flex>
                 </Box>
-                <Box className="p-4 rounded-r-xl">
-                  <ButtonGroup isAttached variant="outline">
-                    <IconButton isActive={view == 'list'} onClick={() => setView('list')} aria-label='List view' icon={<HiViewList/>} />
-                    <IconButton isActive={view == 'grid'} onClick={() => setView('grid')} aria-label='Grid View' icon={<HiViewGrid/>} />
-                  </ButtonGroup>
-                </Box>
-              </Flex>
-            </Box>
-            <div className={`${view == 'grid' && 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'}`}>
-            {results.map((r, index) => (
-                <div key={index} className={`my-2 flex ${view == 'grid' && 'flex-col'} items-center p-2 rounded-md cursor-pointer ${colorMode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-700' }`}>
-                  <img 
-                    src={`${r.Poster != 'N/A' ? r.Poster : 'poster.png'}`} 
-                    className={`${view == 'grid' ? 'w-full' : 'w-10'} rounded-lg mx-2 border border-gray-400`} 
-                    alt={`Poster for ${r.Title}`} 
-                  />
-                  <div className={`flex-grow font-semibold ${view == 'grid' && 'text-center'}`}>{r.Title}</div>
-                  <div className="flex items-center px-5"><PiClockDuotone className="mr-1" /> {r.Year}</div>
-                  <a href={`https://www.imdb.com/title/${r.imdbID}/`} target="_blank">
-                    <div className="flex items-center px-5 ">
-                      <FaImdb className="mr-1" /> {r.imdbID}
+                <div className={`${view == 'grid' && 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'}`}>
+                {results.map((r, index) => (
+                    <div key={index} className={`my-2 flex ${view == 'grid' && 'flex-col'} items-center p-2 rounded-md cursor-pointer ${colorMode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-700' }`}>
+                      <img 
+                        src={`${r.Poster != 'N/A' ? r.Poster : 'poster.png'}`} 
+                        className={`${view == 'grid' ? 'w-full' : 'w-10'} rounded-lg mx-2 border border-gray-400`} 
+                        alt={`Poster for ${r.Title}`} 
+                      />
+                      <div className={`flex-grow font-semibold ${view == 'grid' && 'text-center'}`}>{r.Title}</div>
+                      <div className="flex items-center px-5"><PiClockDuotone className="mr-1" /> {r.Year}</div>
+                      <a href={`https://www.imdb.com/title/${r.imdbID}/`} target="_blank">
+                        <div className="flex items-center px-5 ">
+                          <FaImdb className="mr-1" /> {r.imdbID}
+                        </div>
+                      </a>
                     </div>
-                  </a>
+                ))}
                 </div>
-            ))}
-            </div>
-          </div>
+              </div>
+            }
+          </>
         }
       </div>
     </main>
