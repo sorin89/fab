30a81af6a235ca465cecdf4e5708435e8e84fa1b
Sorin <sorin@q3.ro>
2023-08-14 16:27:03 +0300

    results bar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { FaImdb } from "react-icons/fa";
 import { BiSun, BiMoon } from "react-icons/bi";
 import { PiClockDuotone } from "react-icons/pi";
-import { Button, ButtonGroup, Input, Stack, Text, useColorMode, HStack } from '@chakra-ui/react'
+import { Button, ButtonGroup, Input, Stack, Text, useColorMode, HStack, Box, Flex, Select } from '@chakra-ui/react'
 import Image from 'next/image'
 
 interface Movie {
@@ -33,14 +33,14 @@ export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode()
 
   return (
-    <main className="max-w-5xl mx-auto py-20">
+    <main className="max-w-5xl py-20 mx-4 lg:mx-auto">
       <HStack spacing="10" className="mb-10">
         <Text fontSize="4xl" as="b">Tidybase</Text>
         <Button onClick={toggleColorMode} className="text-3xl">
           {colorMode === 'light' ? <BiSun/> : <BiMoon/> }
         </Button>
       </HStack>
-      <Stack spacing={8} direction='row'>
+      <Stack spacing={4} direction='row' className="items-center">
         <Text>Examples:</Text>
         <Button variant="solid" className="rounded-xl border border-gray-400" onClick={() => search('Matrix')}>Matrix</Button>
         <Button className="rounded-xl border border-gray-400" onClick={() => search('Matrix Reloaded')}>Matrix Reloaded</Button>
@@ -56,21 +56,39 @@ export default function Home() {
         className="my-10"
       />
       <div>
-        {results && results.map((r, index) => (
-          <div key={index}>
-            <div className="my-2 flex items-center p-2 hover:bg-gray-200 dark:hover:bg-gray-200 rounded-md cursor-pointer">
-              <img src={`${r.Poster != 'N/A' ? r.Poster : 'poster.png'}`} className="w-10 h-10 rounded-lg mr-2 border border-gray-400" alt={`Poster for ${r.Title}`} />
-              <div className="flex-grow">{r.Title}</div>
-              <div className="flex-initial px-5"><PiClockDuotone /> {r.Year}</div>
-              <div className="flex-initial px-5">
-                <a href={`https://www.imdb.com/title/${r.imdbID}/`} target="_blank">
-                  <span className="inline-block"><FaImdb /> {r.imdbID}</span>
-                </a>
+        {results && results.length != 0 &&
+          <div>
+            <Box className="rounded-xl">
+              <Flex >
+                <Box bg={colorMode === 'light' ? 'gray.200' : 'gray.700' } flex="1" className="p-4 rounded-l-xl">{results.length} results</Box>
+                <Box bg={colorMode === 'light' ? 'gray.200' : 'gray.700' } className="border p-4 rounded-r-xl">
+                  <Text>Order by:</Text>
+                  <Select>
+                    <option>Default</option>
+                    <option>Title (A&raquo;Z)</option>
+                    <option>Year</option>
+                    <option>Type</option>
+                  </Select>
+                </Box>
+              </Flex>
+            </Box>
+            {results.map((r, index) => (
+              <div key={index}>
+                <div className={`my-2 flex items-center p-2 rounded-md cursor-pointer ${colorMode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-700' }`}>
+                  <img src={`${r.Poster != 'N/A' ? r.Poster : 'poster.png'}`} className="w-10 h-12 rounded-lg mr-2 border border-gray-400" alt={`Poster for ${r.Title}`} />
+                  <div className="flex-grow">{r.Title}</div>
+                  <div className="flex-initial px-5"><PiClockDuotone /> {r.Year}</div>
+                  <div className="flex-initial px-5">
+                    <a href={`https://www.imdb.com/title/${r.imdbID}/`} target="_blank">
+                      <span className="inline-block"><FaImdb /> {r.imdbID}</span>
+                    </a>
+                  </div>
+                  <div className="pl-4 cursor-move text-2xl text-gray-400 hover:text-gray-700 drag"><i className="bi bi-list"/></div>
+                </div>
               </div>
-              <div className="pl-4 cursor-move text-2xl text-gray-400 hover:text-gray-700 drag"><i className="bi bi-list"/></div>
-            </div>
+            ))}
           </div>
-        ))}
+        }
       </div>
     </main>
   )
