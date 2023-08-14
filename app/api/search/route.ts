4d76a94e45e8bd990adb255644a1adb5e16a2cd9
Sorin <sorin@q3.ro>
2023-08-14 12:03:38 +0300

    use NextRequest

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
  
-export async function GET(request: { url: string | URL; }) {
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const q = searchParams.get('q')
   const response = await fetch(`https://www.omdbapi.com/?s=${q}&apikey=${process.env.API_KEY}`);
