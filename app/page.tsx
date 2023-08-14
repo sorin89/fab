93611bfad81086750411e05662fc383613cfb95a
Sorin <sorin@q3.ro>
2023-08-15 00:36:02 +0300

    UX

--- app/page.tsx
+++ app/page.tsx
@@ -34,19 +34,17 @@ export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode()
 
   return (
-    <main className="max-w-5xl py-20 mx-4 lg:mx-auto">
-      <HStack spacing="10" className="mb-10">
-        <Text fontSize="4xl" as="b">Tidybase</Text>
-        <Button onClick={toggleColorMode} className="text-3xl">
-          {colorMode === 'light' ? <BiSun/> : <BiMoon/> }
-        </Button>
+    <main className="max-w-5xl py-10 mx-4 lg:mx-auto">
+      <HStack spacing="10" className="mb-10 text-center">
+        <Text fontSize="4xl" as="b" className="cursor-default">Tidybase</Text>
+        <IconButton aria-label="Color mode" onClick={toggleColorMode} className="text-3xl rounded-full" icon={colorMode === 'light' ? <BiSun/> : <BiMoon/> } />
       </HStack>
-      <Stack spacing={4} direction='row' className="items-center">
-        <Text>Examples:</Text>
-        <Button variant="solid" className="rounded-xl border" onClick={() => search('Matrix')}>Matrix</Button>
-        <Button className="rounded-xl border" onClick={() => search('Matrix Reloaded')}>Matrix Reloaded</Button>
-        <Button className="rounded-xl border" onClick={() => search('Matrix Revolutions')}>Matrix Revolutions</Button>
-      </Stack>
+      <div className="flex flex-wrap items-center">
+        <Text className="mr-2">Examples: </Text>
+        <Button size="sm" className="rounded-xl border mx-1 my-1" onClick={() => search('Matrix')}>Matrix</Button>
+        <Button size="sm" className="rounded-xl border mx-1 my-1" onClick={() => search('Matrix Reloaded')}>Matrix Reloaded</Button>
+        <Button size="sm" className="rounded-xl border mx-1 my-1" onClick={() => search('Matrix Revolutions')}>Matrix Revolutions</Button>
+      </div>
       <Input 
         placeholder="🔍 search"
         type="search"
@@ -61,7 +59,7 @@ export default function Home() {
           <div>
             <Box className="rounded-xl" bg={colorMode === 'light' ? 'gray.50' : 'gray.700' }>
               <Flex>
-                <Box flex="1" className="p-4">{results.length} results</Box>
+                <Box flex="1" className="flex p-4 items-center">{results.length} results</Box>
                 <Box className="p-4">
                   <HStack>
                     <Select icon={<BiSortAlt2/>}>
@@ -81,7 +79,7 @@ export default function Home() {
                 </Box>
               </Flex>
             </Box>
-            <div className={`${view == 'grid' && 'grid grid-cols-2 sm:grid-cols-4 gap-4'}`}>
+            <div className={`${view == 'grid' && 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'}`}>
             {results.map((r, index) => (
                 <div key={index} className={`my-2 flex ${view == 'grid' && 'flex-col'} items-center p-2 rounded-md cursor-pointer ${colorMode === 'light' ? 'hover:bg-gray-200' : 'hover:bg-gray-700' }`}>
                   <img 
