d7cdd6c5a4ddafc0f41b56e8816af999049046ea
Sorin <sorin@q3.ro>
2023-08-16 14:18:15 +0300

    change build step, change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from "./providers"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Movie db',
+  title: 'Tidybase',
   description: 'search for movies',
 }
 
